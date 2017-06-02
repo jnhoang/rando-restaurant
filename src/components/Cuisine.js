2d577f408930a900f2f997992a7ff5e6922bac65
CannedYeti <cpotebnya@gmail.com>
2017-06-02 10:13:38 -0700

    added delete function. need to fix

--- src/components/Cuisine.js
+++ src/components/Cuisine.js
@@ -9,13 +9,21 @@ class Cuisine extends Component {
 
   }
 
+  deleteCuisine(cuisine) {
+    let temp = this.state.cuisines;
+    let index = temp.indexOf(cuisine)
+    temp.splice(index,1)
+    console.log("Cuisine:", cuisine)
+    this.setState = {cuisines: temp};
+    console.log("State:", this.state); 
+  }
 
   render () {
     return (
       <div>
         <h1>Cuisine</h1>
         <ul>
-          {this.state.cuisines.map( (food, index) => <li key={index}><button name={food} onClick={ (e) => console.log(e.target.name) }>{food}</button></li> )}
+          {this.state.cuisines.map( (food, index) => <li key={index}><button name={food} onClick={ (e) => this.deleteCuisine(e.target.name) }>{food}</button></li> )}
         </ul>
       </div>
     );
