8980dd69a2eaff0651b5525394202eecd3f568bf
jnhustin <jnhustin@gmail.com>
2017-06-02 10:52:19 -0700

    successful giphy calls

--- src/components/Cuisine.js
+++ src/components/Cuisine.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
 
-
+// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+const giphyAPIKey = '&api_key=dc6zaTOxFJmzC';
+const giphyLink = 'http://api.giphy.com/v1/gifs/search?q=';
 
 class Cuisine extends Component {
   constructor() {
     super();
     this.state = {
       cuisines: ["Mexican", "Chinese", "Japanese", "Thai", "American", "Indian", "Soul", "Cambodian", "French", "Greek", "Vietnamese"]
+    , images: []
     };
 
   }
+  componentDidMount() {
+    let cuisines = this.state.cuisines;
+    var results = this.state.images;
 
+    cuisines.map( (food) => {
+      fetch(giphyLink + food + ' food' + giphyAPIKey)
+      .then( (res) =>  res.json() )
+      .then( (json) => {
+        results.push(json.data[0].images.fixed_height.url);
+        // console.log(results);
+        console.log('res: ', json.data[0].images.fixed_height.url)
+        
+        this.setState({
+          images: results
+        });
+      })
+      .catch( (err) => console.log('error:', err) );   
+    })
+  }
   deleteCuisine(cuisine) {
     let temp = this.state.cuisines;
     let index = temp.indexOf(cuisine);
@@ -24,8 +45,9 @@ class Cuisine extends Component {
       <div>
         <h1>Cuisine</h1>
         <ul>
-          {this.state.cuisines.map( 
-            (food, index) => <li key={index}><button name={food} onClick={ (e) => this.deleteCuisine(e.target.name) }>{food}</button></li> )}
+          {this.state.images.map( (img, index) => <img key={index} src={img} alt=""/>)}
+          
+          {this.state.cuisines.map( (food, index) => <li key={index}><button name={food} onClick={ (e) => this.deleteCuisine(e.target.name) }>{food}</button></li> )}
         </ul>
       </div>
     );
