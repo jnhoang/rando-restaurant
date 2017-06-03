456f5540f3f1f264d236d0c1a977bd11a12706d6
jnhustin <jnhustin@gmail.com>
2017-06-03 13:52:49 -0700

    refactor delete cuisine button

--- src/components/Cuisine.js
+++ src/components/Cuisine.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 const giphyAPIKey = '&api_key=dc6zaTOxFJmzC';
 const giphyLink = 'http://api.giphy.com/v1/gifs/search?q=';
 
@@ -14,41 +13,59 @@ class Cuisine extends Component {
 
   }
   componentDidMount() {
-    let cuisines = this.state.cuisines;
-    var results = this.state.images;
+    let cuisines  = this.state.cuisines;
+    let results   = this.state.images;
 
+    // giphy call for each choice item in cuisine array
     cuisines.map( (food) => {
       fetch(giphyLink + food + ' food' + giphyAPIKey)
-      .then( (res) =>  res.json() )
+      .then( (res)  =>  res.json() )
       .then( (json) => {
-        results.push({[food]: json.data[0].images.fixed_height.url});
-        // console.log(results);
-        console.log('res: ', results)
-        
+        // adds the resulting image to a state arr
+        results.push({[food]: json.data[0].images.fixed_height.url});    
         this.setState({
           images: results
         });
       })
-      .catch( (err) => console.log('error:', err) );   
+      .catch( (err) => console.log('error: ', err) );   
     })
   }
   deleteCuisine(cuisine) {
-    let temp = this.state.cuisines;
-    let index = temp.indexOf(cuisine);
+    let temp = this.state.cuisines.filter( (food) => food !== cuisine);
 
-    temp.splice(index,1);
-    this.setState({'cuisines': temp});
+    this.setState({cuisines: temp});
   }
+  handleReset() {
 
+  }
+  handleRandom() {
+
+  }
   render () {
     return (
       <div>
         <h1>Cuisine</h1>
         <ul>
-          {this.state.images.map( (obj, index) => <li key={index}><img src={obj[Object.keys(obj)]}/><button name={Object.keys(obj)} onClick={ (e) => this.deleteCuisine(e.target.name) }>{Object.keys(obj)}</button></li> )}
-
-          {/*{this.state.cuisines.map( (food, index) => <li key={index}><img src={this.state.images[index][food]}/><button name={food} onClick={ (e) => this.deleteCuisine(e.target.name) }>{food}</button></li> )}*/}
+          {this.state.images.map( (obj, index) => 
+            <li key={index}>
+              <img src={obj[Object.keys(obj)]}/>
+              <button 
+                name={Object.keys(obj)} 
+                onClick={ (e) => this.deleteCuisine(e.target.name) }>
+                { Object.keys(obj) }
+              </button>
+            </li> 
+          )}
         </ul>
+
+        <button
+          onClick={ (e) => this.handleReset(e) }>
+          Reset
+        </button>
+        <button
+          onClick={ (e) => this.handleRandom(e) }>
+          Reset
+        </button>
       </div>
     );
   }
